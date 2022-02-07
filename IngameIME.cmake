2497748912fb6c685f22eff83e60670a8b4e186c
Windmill-City <1449182174@qq.com>
2022-02-07 20:37:58 +0800

    Use cmake function, improve flexiblity

---
+++ IngameIME.cmake
@@ -0,0 +1,71 @@
+
+#[[
+Add a new IngameIME C++ Library target
+
+@param Target - Target name of the library
+@param Type - Library type<SHARED|STATIC|MODULE>
+@param Source - Source file of the library to compile
+]]
+function(IngameIME_add_library Target Type Source)
+    message(STATUS "Adding ${Target} as ${Type} Library with source ${Source}")
+    add_library(${Target} ${Type} ${Source})
+    target_include_directories(${Target} PRIVATE ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/src/include)
+endfunction()
+
+#[[
+Add a new IngameIME SWIG language binding library target
+
+Currently supported language:
+- CSharp
+- Java
+- Python
+
+@param Target - Target name of the library
+@param Type - Library type<SHARED|STATIC|MODULE>
+@param Language - Target language of the binding
+
+@note The path of the generated binding files can be retrieved from the SWIG_SUPPORT_FILES_DIRECTORY target property.
+@code{
+    get_property(support_files_dir TARGET targetName PROPERTY SWIG_SUPPORT_FILES_DIRECTORY)
+}
+]]
+function(IngameIME_add_swig_library Target Type Language)
+    message(STATUS "Adding ${Target} as SWIG ${Type} Library for Language ${Language}")
+    find_package(SWIG 4.0.2 REQUIRED)
+    set(UseSWIG_MODULE_VERSION 2)
+
+    INCLUDE(${SWIG_USE_FILE})
+    SET_SOURCE_FILES_PROPERTIES(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/src/IngameIME.i PROPERTIES CPLUSPLUS ON)
+
+    # Language Name must in lower case
+    string(TOLOWER ${Language} ${Language})
+
+    swig_add_library(${Target} TYPE ${Type} LANGUAGE ${Language} SOURCES ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/src/IngameIME.i)
+
+    set_property(TARGET ${Target} PROPERTY SWIG_USE_TARGET_INCLUDE_DIRECTORIES ON)
+    target_include_directories(${Target} PUBLIC ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/src/include)
+    
+    # Log about support files
+    get_property(SUPPORT_FILES_DIR TARGET ${Target} PROPERTY SWIG_SUPPORT_FILES_DIRECTORY)
+    message(STATUS "[${Target}] Support files will be place at: ${SUPPORT_FILES_DIR}")
+    
+    #Java Binding
+    if(${Language} STREQUAL "java")
+        find_package(JNI REQUIRED)
+
+        target_include_directories(${Target} PUBLIC ${JNI_INCLUDE_DIRS})
+        target_link_directories(${Target} PUBLIC ${JNI_LIBRARIES})
+        # Generate document in generated codes
+        set_property(TARGET ${Target} PROPERTY SWIG_COMPILE_OPTIONS -doxygen)
+    endif()
+
+    #Python Binding
+    if(${Language} STREQUAL "python")
+        find_package (Python3 REQUIRED COMPONENTS Interpreter Development)
+
+        target_include_directories(${Target} PUBLIC ${Python3_INCLUDE_DIRS})
+        target_link_directories(${Target} PUBLIC ${Python3_LIBRARIES})
+        # Generate document in generated codes
+        set_property(TARGET ${Target} PROPERTY SWIG_COMPILE_OPTIONS -doxygen)
+    endif()
+endfunction()
