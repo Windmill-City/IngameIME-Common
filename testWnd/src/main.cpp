bb103e1fe5b5254d60c6a97dde1fbdb405eef62f
Windmill_City <1449182174@qq.com>
2023-01-29 17:55:30 +0800

    basic glfw window

--- testWnd/src/main.cpp
+++ testWnd/src/main.cpp
@@ -1,45 +1,60 @@
+#include <cstdlib>
 #include <iostream>
-#include <string>
 
 #include <glad/glad.h>
-#include <glfw/glfw3.h>
+#include <GLFW/glfw3.h>
 
-#include "InputWindow.hpp"
-
-void error_callback(int error, const char* description)
-{
-    std::cout << "Error:" << error << description << std::endl;
-}
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 int main()
 {
-    glfwSetErrorCallback(error_callback);
-    if (glfwInit())
+    // GLFW Init Start
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#if defined(__APPLE__)
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    // GLFW Init End
+
+    // Create Window Start
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (!window)
     {
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-        // Must call once to set active locale
-        setlocale(LC_ALL, "");
-
-        try
-        {
-            auto window = std::make_unique<InputWindow>();
-            while (window->pollEvent())
-                ;
-        }
-        catch (std::exception& e)
-        {
-            std::cerr << e.what() << '\n';
-        }
-        catch (...)
-        {
-            std::cerr << "err occurs!" << '\n';
-        }
-
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
-        return 0;
+
+        std::exit(EXIT_FAILURE);
     }
-    return -1;
+    glfwMakeContextCurrent(window);
+    // Create Window End
+
+    // GLAD Init Start
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    // GLAD Init End
+
+    // Event Loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Background color
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    std::exit(EXIT_SUCCESS);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
 }
