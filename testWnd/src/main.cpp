a9d8960478f64b41f85b6626e1b57d8e3fa3a701
Windmill_City <1449182174@qq.com>
2023-02-01 11:16:54 +0800

    CJK font & TextEdit

--- testWnd/src/main.cpp
+++ testWnd/src/main.cpp
@@ -10,7 +10,6 @@
 #define NK_INCLUDE_DEFAULT_ALLOCATOR
 #define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
 #define NK_INCLUDE_FONT_BAKING
-#define NK_INCLUDE_DEFAULT_FONT
 #define NK_BUTTON_TRIGGER_ON_RELEASE
 #define NK_KEYSTATE_BASED_INPUT
 #define NK_IMPLEMENTATION
@@ -21,6 +20,27 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 struct nk_context* nk;
+
+/**
+ * @brief Range of commonly used CJK charactors, if there still missing charactors,
+ * refer to http://www.unicode.org/charts/ and fix the table
+ */
+// clang-format off
+
+const nk_rune ranges[] = {
+    0x0370, 0x03FF, // Greek
+    0x0020, 0x007F, // Basic Latin(ASCII)
+    0x3000, 0x303F, // CJK Symbols and Punctuation
+    0xFF00, 0xFFEF, // Halfwidth and Fullwidth Forms
+    0x4E00, 0x9FFF, // CJK Unified Ideographs (Han)
+    0x1100, 0x11FF, // Hangul Jamo
+    0xAC00, 0xD7AF, // Hangul Syllables
+    0x3040, 0x309F, // Hiragana
+    0x30A0, 0x30FF, // Katakana
+    0
+};
+
+// clang-format on
 
 int main()
 {
@@ -68,9 +88,18 @@ int main()
     nk = nk_glfw3_init(window, NK_GLFW3_INSTALL_CALLBACKS, 512 * 1024, 128 * 1024);
 
     // Load Font
+    struct nk_font_config config = nk_font_config(0);
+    config.range                 = ranges;
+    /* align every character to pixel boundary (if true set oversample (1,1)) */
+    config.oversample_h          = 1;
+    config.oversample_v          = 1;
+    config.pixel_snap            = true;
+
     struct nk_font_atlas* atlas;
     nk_glfw3_font_stash_begin(&atlas);
+    struct nk_font* font = nk_font_atlas_add_from_file(atlas, "assets/fonts/NotoSansCJKsc-VF.ttf", 20, &config);
     nk_glfw3_font_stash_end();
+    nk_style_set_font(nk, &font->handle);
     // Nuklear Init End
 
     // Event Loop
@@ -85,6 +114,10 @@ int main()
                      nk_rect(wnd_w / 2 - main_w / 2, wnd_h / 2 - main_h / 2, main_w, main_h),
                      NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_TITLE))
         {
+            // Press ESC to exit the window
+            if (glfwGetKey(window, GLFW_KEY_ESCAPE)) glfwSetWindowShouldClose(window, GLFW_TRUE);
+
+            // Toggle Fullscreen
             nk_layout_row_dynamic(nk, 30, 1);
             if (nk_button_label(nk, "Toggle Fullscreen"))
             {
@@ -101,6 +134,15 @@ int main()
                     glfwSetWindowMonitor(window, monitor, 0, 0, mode->width, mode->height, mode->refreshRate);
                 }
             }
+
+            // TextEdit
+            static char buffer[256];
+            nk_layout_row_dynamic(nk, 30, 1);
+            nk_edit_string_zero_terminated(nk,
+                                           NK_EDIT_FIELD | NK_EDIT_SIG_ENTER | NK_EDIT_GOTO_END_ON_ACTIVATE,
+                                           buffer,
+                                           sizeof(buffer),
+                                           nk_filter_default);
         }
         nk_end(nk);
         // Nuklear Window End
